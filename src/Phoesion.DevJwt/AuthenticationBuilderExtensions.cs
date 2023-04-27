91f6a75208db017bc1de62374a86cf8298701771
George Papadopoulos <gepa@phoesion.com>
2023-04-27 08:27:35 +0300

    fix allowed_environments check

--- src/Phoesion.DevJwt/AuthenticationBuilderExtensions.cs
+++ src/Phoesion.DevJwt/AuthenticationBuilderExtensions.cs
@@ -27,16 +27,19 @@ namespace Microsoft.Extensions.DependencyInjection
         /// <param name="key">Specify a custom key for checking key signature</param>
         /// <param name="allowed_environments">Environment names to allow the DevJwt to be enabled</param>
         /// <returns>True if the DevJwt validator has been added. False if no changes have been made</returns>
-        public static bool UseDevJwt(this Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerOptions options, IHostEnvironment env, string key = null, string[] allowed_environments = null)
+        public static bool UseDevJwt(this Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerOptions options, IHostEnvironment env, string signkey = null, string[] allowed_environments = null)
         {
             //check if valid environment
-            if (allowed_environments != null && !allowed_environments.Contains(env.EnvironmentName))
-                return false;
+            if (allowed_environments != null)
+            {
+                if (!allowed_environments.Contains(env.EnvironmentName))
+                    return false;
+            }
             else if (!(env.IsDevelopment() || env.EnvironmentName == "Testing"))
                 return false;
 
             //add custom validator
-            options.SecurityTokenValidators.Insert(0, new TokenValidator(key));
+            options.SecurityTokenValidators.Insert(0, new TokenValidator(signkey));
 
             //add valid issuers
             var issuers = options.TokenValidationParameters.ValidIssuers?.ToHashSet() ?? new();
